986956571b77ae53d20a65916ef7881a306b2f54
Casudo <sudfwd+github@gmail.com>
2023-11-25 00:17:29 +0100

    Added Get Team Player function and class

---
+++ kickbase/competition.py
@@ -0,0 +1,62 @@
+"""
+### This module holds all necessary functions to call Kickbase `/competition/...` API endpoints.
+
+TODO: Maybe list all functions here automatically?
+"""
+
+import requests
+from kickbase import exceptions
+
+from kickbase.endpoints.competition import Team_Players
+
+
+def team_players(token: str, team_id: str):
+    """
+    Get all players of a given team
+    
+    Expected response:
+    ```json
+    {
+        "p": [
+            {
+                "id": "237",
+                "teamId": "2",
+                "teamName": "Bayern",
+                "teamSymbol": "FCB",
+                "firstName": "Manuel",
+                "lastName": "Neuer",
+                "profile": "https://kickbase.b-cdn.net/pool/players/237.jpg",
+                "profileBig": "https://kickbase.b-cdn.net/pool/playersbig/237.png",
+                "team": "https://kickbase.b-cdn.net/team/2013/07/30/3ebe44c53c3f4c87bd605da69e743fb8.jpg",
+                "teamCover": "https://kickbase.b-cdn.net/team/2013/08/01/ec8377d89197450e89fa942e4e36d48c.png",
+                "status": 0,
+                "position": 1,
+                "number": 1,
+                "averagePoints": 127,
+                "totalPoints": 381,
+                "marketValue": 19422256.0,
+                "marketValueTrend": 1
+            },
+            { ... },
+        ]
+    }
+    ```
+    """
+    url = f"https://api.kickbase.com/competition/teams/{team_id}/players"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Cookie": f"kkstrauth={token};",
+    }
+
+    ### Send GET request to get all players of a given team
+    try:
+        response = requests.get(url, headers=headers).json()
+    except:
+        raise exceptions.NotificatonException("Notification failed! Please check your Discord Webhook URL.") # TODO: Change exception
+
+    ### Create a new object for every entry in the response["p"] list.
+    ### response["p"] holds all players of the given team.
+    players_in_team = [Team_Players(player) for player in response["p"]]
+
+    return players_in_team
--- kickbase/endpoints/competition.py
+++ kickbase/endpoints/competition.py
@@ -5,6 +5,98 @@ Endpoint:
 `/competition/...`
 """
 
+
+### ===============================================================================
+### Team Players stuff
+
+class P:
+    """
+    ### Create an object for the team players with all its attributes.
+
+    The JSON response for the feed entries looks like this:
+    ```json  
+    {
+        "p": [
+            {
+                "id": "237",
+                "teamId": "2",
+                "teamName": "Bayern",
+                "teamSymbol": "FCB",
+                "firstName": "Manuel",
+                "lastName": "Neuer",
+                "profile": "https://kickbase.b-cdn.net/pool/players/237.jpg",
+                "profileBig": "https://kickbase.b-cdn.net/pool/playersbig/237.png",
+                "team": "https://kickbase.b-cdn.net/team/2013/07/30/3ebe44c53c3f4c87bd605da69e743fb8.jpg",
+                "teamCover": "https://kickbase.b-cdn.net/team/2013/08/01/ec8377d89197450e89fa942e4e36d48c.png",
+                "status": 0,
+                "position": 1,
+                "number": 1,
+                "averagePoints": 127,
+                "totalPoints": 381,
+                "marketValue": 19422256.0,
+                "marketValueTrend": 1
+            },
+            { ... },
+        ]
+    }
+    ```
+    """
+    id: str = None
+    teamId: str = None
+    teamName: str = None
+    teamSymbol: str = None
+    firstName: str = None
+    lastName: str = None
+    profile: str = None # Somehow not always present e.g. Jamal Musiala
+    profileBig: str = None
+    team: str = None # Somehow not always present
+    teamCover: str = None # Somehow not always present
+    status: int = None
+    position: int = None
+    number: int = None
+    averagePoints: int = None
+    totalPoints: int = None
+    marketValue: float = None
+    marketValueTrend: int = None
+
+    def __init__(self, p_dict: dict):
+        self.id = p_dict["id"]
+        self.teamId = p_dict["teamId"]
+        self.teamName = p_dict["teamName"]
+        self.teamSymbol = p_dict["teamSymbol"]
+        self.firstName = p_dict["firstName"]
+        self.lastName = p_dict["lastName"]
+        if "profile" in p_dict:
+            self.profile = p_dict["profile"]
+        self.profileBig = p_dict["profileBig"]
+        if "team" in p_dict:
+            self.team = p_dict["team"]
+            self.teamCover = p_dict["teamCover"]
+        self.status = p_dict["status"]
+        self.position = p_dict["position"]
+        self.number = p_dict["number"]
+        self.averagePoints = p_dict["averagePoints"]
+        self.totalPoints = p_dict["totalPoints"]
+        self.marketValue = p_dict["marketValue"]
+        self.marketValueTrend = p_dict["marketValueTrend"]
+
+
+class Team_Players:
+    """
+    ### Create an object for the team players with all its attributes.
+
+    Used for endpoint URL:
+    `/competition/teams/[team_id]/players` -> get all players of a given team
+    """
+    ### No attributes here!
+
+    def __init__(self, team_players_dict: dict):
+        self.p = P(team_players_dict)
+
+### End of Team Players stuff
+### ===============================================================================
+
+
 # class Search_Competition_Players: 
 #     """
 #     ### TODO: Change me
@@ -35,14 +127,6 @@ Endpoint:
 #
 #     Used for endpoint URL:
 #     `/competition/table?matchDay=<MATCH_DAY_NR>` -> TODO: Change me
-#     """
-
-# class Team_Players: 
-#     """
-#     ### TODO: Change me
-#
-#     Used for endpoint URL:
-#     `/competition/teams/[team_id]/players` -> TODO: Change me
 #     """
 
 # class Search_Players: 
