b63d1e6670bcef4e7bb29d4ea8c67014b30ed664
Casudo <sudfwd+github@gmail.com>
2023-11-25 00:18:24 +0100

    Added constants

--- kickbase/miscellaneous.py
+++ kickbase/miscellaneous.py
@@ -1,5 +1,5 @@
 """
-### This module holds all functions that are not related to Kickbase API in any point.
+### This module holds all functions and constants that are not related to Kickbase API in any point.
 
 TODO: Maybe list all functions here automatically?
 """
@@ -7,6 +7,38 @@ TODO: Maybe list all functions here automatically?
 import requests
 from kickbase import exceptions
 
+### ===============================================================================
+
+POSITIONS = {1: 'TW', 2: 'ABW', 3: 'MF', 4: 'ANG'}
+### TREND?
+### STATUS
+### TYPE
+
+### TEAM_IDS
+### TODO: Update with missing teams
+# 2 Bayern
+# 3 BVB
+# 4 Frankfurt
+# 5 Freiburg
+# 7 Bayer
+# 8 Schalke
+# 9 Stuttgart
+# 10 Bremen
+# 11 Wolfsburg
+# 13 Augsburg
+# 14 Hoffenheim
+# 15 Gladbach
+# 18 Mainz
+# 20 Hertha
+# 24 Bochum
+# 28 Köln
+# 40 Union
+# 42 Darmstadt
+# 43 Leipzig
+# 50 Heidenheim
+TEAM_IDS = [2, 3, 4, 5, 7, 9, 10, 11, 13, 14, 15, 18, 24, 28, 40, 42, 43, 50]
+
+### ===============================================================================
 
 def discord_notification(title: str, message: str, color: int):
     """
