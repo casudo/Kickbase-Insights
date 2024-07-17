14b4e2597449ca90a63d60ced14a3745cbb6556b
Casudo <sudfwd+github@gmail.com>
2024-07-17 11:13:06 +0200

    Updated Team ID and match days + added "Raus aus der Liga" status

--- kickbase/miscellaneous.py
+++ kickbase/miscellaneous.py
@@ -27,7 +27,7 @@ POSITIONS = {1: 'TW', 2: 'ABW', 3: 'MF', 4: 'ANG'}
 # 4: Aufbautraining (Orange Cone)
 # 8: Rote Karte (Red Card)
 # 32: 5. Gelbe Karte (Yellow Card)
-# TODO: Add "Raus aus der Liga"
+# 128: Raus aus der Liga (No Icon yet!)
 ### Conversion from number to icon for the frontend in "SharedConstants.js"
 
 ### TYPE (from league feed)
@@ -38,50 +38,80 @@ POSITIONS = {1: 'TW', 2: 'ABW', 3: 'MF', 4: 'ANG'}
 # Type 12: Gekauft von Kickbase
 
 ### TEAM_IDS
-### TODO: Update with missing teams
+### As of 17.07.2024
+# 1 -
 # 2 Bayern
 # 3 BVB
 # 4 Frankfurt
 # 5 Freiburg
+# 6 - 
 # 7 Bayer
-# 8 Schalke
+# 8 -
 # 9 Stuttgart
 # 10 Bremen
 # 11 Wolfsburg
+# 12 -
 # 13 Augsburg
 # 14 Hoffenheim
 # 15 Gladbach
+# 16 -
+# 17 -
 # 18 Mainz
-# 20 Hertha
+# 19 -
+# 20 -
+# 21 -
+# 22 -
+# 23 -
 # 24 Bochum
-# 28 Köln
+# 25-38 -
+# 39 St. Pauli (Missing Team picture)
 # 40 Union
-# 42 Darmstadt
+# 41 - 
+# 42 -
 # 43 Leipzig
+# 44-49 -
 # 50 Heidenheim
-TEAM_IDS = [2, 3, 4, 5, 7, 9, 10, 11, 13, 14, 15, 18, 24, 28, 40, 42, 43, 50]
+# 51 Holstein Kiel (Missing Team picture)
+TEAM_IDS = [2, 3, 4, 5, 7, 9, 10, 11, 13, 14, 15, 18, 24, 39, 40, 43, 50, 51]
 
 TIMEZONE_DE = pytz.timezone("Europe/Berlin")
 
 ### TODO: Update these manually??
 MATCH_DAYS = {
-    1: datetime(2023, 8, 18, 20, 30, tzinfo=TIMEZONE_DE),
-    2: datetime(2023, 8, 25, 20, 30, tzinfo=TIMEZONE_DE),
-    3: datetime(2023, 9, 1, 20, 30, tzinfo=TIMEZONE_DE),
-    4: datetime(2023, 9, 15, 20, 30, tzinfo=TIMEZONE_DE),
-    5: datetime(2023, 9, 22, 20, 30, tzinfo=TIMEZONE_DE),
-    6: datetime(2023, 9, 29, 20, 30, tzinfo=TIMEZONE_DE),
-    7: datetime(2023, 10, 6, 20, 30, tzinfo=TIMEZONE_DE),
-    8: datetime(2023, 10, 20, 20, 30, tzinfo=TIMEZONE_DE),
-    9: datetime(2023, 10, 27, 20, 30, tzinfo=TIMEZONE_DE),
-    10: datetime(2023, 11, 3, 20, 30, tzinfo=TIMEZONE_DE),
-    11: datetime(2023, 11, 10, 20, 30, tzinfo=TIMEZONE_DE),
-    12: datetime(2023, 11, 24, 20, 30, tzinfo=TIMEZONE_DE),
-    13: datetime(2023, 12, 1, 20, 30, tzinfo=TIMEZONE_DE),
-    14: datetime(2023, 12, 8, 20, 30, tzinfo=TIMEZONE_DE),
-    15: datetime(2023, 12, 15, 20, 30, tzinfo=TIMEZONE_DE),
-    16: datetime(2023, 12, 19, 18, 30, tzinfo=TIMEZONE_DE),
-    17: datetime(2024, 1, 12, 20, 30, tzinfo=TIMEZONE_DE),
+    1: datetime(2024, 8, 23, 20, 30, tzinfo=TIMEZONE_DE),
+    2: datetime(2024, 8, 30, 20, 30, tzinfo=TIMEZONE_DE),
+    3: datetime(2024, 9, 13, 20, 30, tzinfo=TIMEZONE_DE),
+    4: datetime(2024, 9, 20, 20, 30, tzinfo=TIMEZONE_DE),
+    5: datetime(2024, 9, 27, 20, 30, tzinfo=TIMEZONE_DE),
+    6: datetime(2024, 10, 4, 20, 30, tzinfo=TIMEZONE_DE),
+    7: datetime(2024, 10, 18, 20, 30, tzinfo=TIMEZONE_DE),
+    8: datetime(2024, 10, 25, 20, 30, tzinfo=TIMEZONE_DE),
+    9: datetime(2024, 11, 1, 20, 30, tzinfo=TIMEZONE_DE),
+    10: datetime(2024, 11, 8, 20, 30, tzinfo=TIMEZONE_DE),
+    11: datetime(2024, 11, 22, 20, 30, tzinfo=TIMEZONE_DE),
+    12: datetime(2024, 11, 29, 20, 30, tzinfo=TIMEZONE_DE),
+    13: datetime(2024, 12, 6, 20, 30, tzinfo=TIMEZONE_DE),
+    14: datetime(2024, 12, 13, 20, 30, tzinfo=TIMEZONE_DE),
+    15: datetime(2024, 12, 20, 20, 30, tzinfo=TIMEZONE_DE),
+    16: datetime(2025, 1, 10, 18, 30, tzinfo=TIMEZONE_DE),
+    17: datetime(2025, 1, 14, 20, 30, tzinfo=TIMEZONE_DE),
+    18: datetime(2025, 1, 17, 20, 30, tzinfo=TIMEZONE_DE),
+    19: datetime(2025, 1, 24, 20, 30, tzinfo=TIMEZONE_DE),
+    20: datetime(2025, 1, 31, 20, 30, tzinfo=TIMEZONE_DE),
+    21: datetime(2025, 2, 7, 20, 30, tzinfo=TIMEZONE_DE),
+    22: datetime(2025, 2, 14, 20, 30, tzinfo=TIMEZONE_DE),
+    23: datetime(2025, 2, 21, 20, 30, tzinfo=TIMEZONE_DE),
+    24: datetime(2025, 2, 28, 20, 30, tzinfo=TIMEZONE_DE),
+    25: datetime(2025, 3, 7, 20, 30, tzinfo=TIMEZONE_DE),
+    26: datetime(2025, 3, 14, 20, 30, tzinfo=TIMEZONE_DE),
+    27: datetime(2025, 3, 28, 20, 30, tzinfo=TIMEZONE_DE),
+    28: datetime(2025, 4, 4, 20, 30, tzinfo=TIMEZONE_DE),
+    29: datetime(2025, 4, 11, 20, 30, tzinfo=TIMEZONE_DE),
+    30: datetime(2025, 4, 19, 20, 30, tzinfo=TIMEZONE_DE),
+    31: datetime(2025, 4, 25, 20, 30, tzinfo=TIMEZONE_DE),
+    32: datetime(2025, 5, 2, 20, 30, tzinfo=TIMEZONE_DE),
+    33: datetime(2025, 5, 9, 20, 30, tzinfo=TIMEZONE_DE),
+    34: datetime(2025, 5, 17, 15, 30, tzinfo=TIMEZONE_DE),
 }  
 
 
@@ -174,7 +204,7 @@ def calculate_revenue_data_daily(turnovers, manager):
 
     ### Add start and end points for the graph
     for _, data in user_transfer_revenue.items():
-        data.append((0, datetime(2023, 8, 22))) ### TODO: Change Startday at the end of the season ???
+        data.append((0, datetime(2024, 8, 23))) ### TODO: Change Startday at the end of the season ???
         data.append((0, datetime.now()))
 
     ### This section converts the data in user_transfer_revenue into Pandas DataFrames.
