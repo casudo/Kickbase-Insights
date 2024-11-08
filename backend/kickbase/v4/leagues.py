8dfd20086944f770f27d5fa701f4e4e3550453f9
Casudo <sudfwd+github@gmail.com>
2024-11-08 09:49:07 +0100

    Added get_market function for v4 to leagues API

--- backend/kickbase/v4/leagues.py
+++ backend/kickbase/v4/leagues.py
@@ -7,7 +7,7 @@ TODO: Maybe list all functions here automatically?
 import requests
 
 from backend import exceptions
-from backend.kickbase.endpoints.leagues import League_Info
+from backend.kickbase.endpoints.leagues import League_Info, Market_Players
 
 
 def get_league_list(token: str) -> list:
@@ -36,3 +36,39 @@ def get_league_list(token: str) -> list:
     league_list = [League_Info(entry) for entry in json_response["it"]]
 
     return league_list
+
+
+def get_market(token: str, league_id: str):
+    """
+    ### Get the current players on the market in the league
+
+    Expected response:
+    ```json
+    {
+        "it": [ ... ],
+        "nps": 41,
+        "tv": 69420,
+        "mvud": "2023-11-24T21:00:00Z",
+        "dt": "2023-11-24T19:30:00Z",
+        "day": 12   
+    }
+    ```
+    Obviously the "it" list is filled with all players on the market.
+    """
+    url = f"https://api.kickbase.com/v4/leagues/{league_id}/market"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Cookie": f"kkstrauth={token};",
+    }
+
+    ### Send GET request to get all free players in the given league
+    try:
+        json_response = requests.get(url, headers=headers).json()
+    except:
+        raise exceptions.NotificatonException("Notification failed! Please check your Discord Webhook URL.") # TODO: Change exception
+    
+    ### Create a new object for every entry in the json_response["it"] list.
+    players_on_market = [Market_Players(player) for player in json_response["it"]]
+
+    return players_on_market
